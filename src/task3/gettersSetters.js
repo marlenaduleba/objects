fef959ec30d176e8c6783b8a79f3c28aebc005b3
Marlena Duleba <marlena_duleba@o2.pl>
2024-04-25 14:56:05 +0200

    Add task3 solution plus tests

---
+++ src/task3/gettersSetters.js
@@ -0,0 +1,23 @@
+const bankAccount = {
+  _balance: 1000,
+  get balance() {
+    return this._balance;
+  },
+  set balance(value) {
+    this._balance = value;
+    this.formattedBalance = `$${value}`;
+  },
+  get formattedBalance() {
+    return `$${this._balance}`;
+  },
+  transfer(target, amount) {
+    if (this.balance >= amount) {
+      this.balance -= amount;
+      target.balance += amount;
+    } else {
+        console.log("Insufficient funds. Transfer cannot be completed.");
+    }
+  },
+};
+
+module.exports = { bankAccount };
